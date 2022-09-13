b3421dadf4342202620392dd0e1d31d45b834d1c
Ксения Тютчева <dmitrievna13@mail.ru>
2022-09-13 19:33:50 +0700

    Commit reformat code

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -11,12 +11,12 @@ public class Manager {
     }
 
     public void add(String film) {
-        String[] tmp = new String[films.length+1];
+        String[] tmp = new String[films.length + 1];
         for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
         }
-            tmp[tmp.length - 1] = film;
-            films = tmp;
+        tmp[tmp.length - 1] = film;
+        films = tmp;
 
 
     }
@@ -26,18 +26,18 @@ public class Manager {
 
     }
 
-   public String[] findLast () {
+    public String[] findLast() {
         int resultLenght;
         if (films.length >= limit) {
             resultLenght = limit;
         } else {
             resultLenght = films.length;
         }
-        String [] result = new String[resultLenght];
-       for (int i = 0; i < result.length; i++) {
-           result[i] = films[films.length - 1 - i];
-       }
-       return result;
+        String[] result = new String[resultLenght];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = films[films.length - 1 - i];
+        }
+        return result;
 
     }
 
